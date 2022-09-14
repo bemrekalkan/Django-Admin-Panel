06bc5073ba916c9f63167d3258d3deed309ce88c
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-14 20:29:31 +0300

    admin

---
+++ products/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Product
+
+admin.site.register(Product)
+
+admin.site.site_title = "Clarusway Title"
+admin.site.site_header = "Clarusway Admin Portal"
+admin.site.index_title = "Welcome to Clarusway Admin Portal"
