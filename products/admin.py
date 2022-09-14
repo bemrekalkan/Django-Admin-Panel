08998a7154e7ec561a771abf486bd494349e3c81
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-14 20:52:34 +0300

    ProductAdmin added

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,34 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "is_in_stock", "update_date")
+    list_editable = ( "is_in_stock", )
+    # list_display_links = ("create_date", ) #can't add items in list_editable to here
+    list_filter = ("is_in_stock", "create_date")
+    ordering = ("name",)
+    search_fields = ("name",)
+    prepopulated_fields = {'slug' : ('name',)}   # when adding product in admin site
+    list_per_page = 25
+    date_hierarchy = "update_date"
+    # fields = (('name', 'slug'), 'description', "is_in_stock") #fieldset kullandığımız zaman bunu kullanamayız
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('name', 'slug'), "is_in_stock" # to display multiple fields on the same line, wrap those fields in their own tuple
+            ),
+            # 'classes': ('wide', 'extrapretty'), wide or collapse
+        }),
+        ('Optionals Settings', {
+            "classes" : ("collapse", ),
+            "fields" : ("description",),
+            'description' : "You can use this section for optionals settings"
+        })
+    )
+
+admin.site.register(Product, ProductAdmin)
 
 admin.site.site_title = "Clarusway Title"
 admin.site.site_header = "Clarusway Admin Portal"
